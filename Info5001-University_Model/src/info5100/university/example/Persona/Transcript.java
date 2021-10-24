52ce0651868c34507f69e0a118c4e2b01e3fcec6
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2021-10-24 17:49:08 -0400

    Group Assignment AED Package Upload

---
+++ Info5001-University_Model/src/info5100/university/example/Persona/Transcript.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info5100.university.example.Persona;
+
+import info5100.university.example.CourseSchedule.CourseLoad;
+import info5100.university.example.CourseSchedule.SeatAssignment;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author kal bugrara
+ */
+
+public class Transcript {
+
+    StudentProfile student;
+    HashMap<String, CourseLoad> courseloadlist;
+
+    CourseLoad currentcourseload;
+
+    public Transcript(StudentProfile sp) {
+        student = sp;
+        courseloadlist = new HashMap();
+
+    }
+
+    public CourseLoad newCourseLoad(String sem) {
+
+        currentcourseload = new CourseLoad(sem);
+        courseloadlist.put(sem, currentcourseload);
+        return currentcourseload;
+    }
+
+    public CourseLoad getCurrentCourseLoad() {
+
+        return currentcourseload;
+
+    }
+
+    public CourseLoad getCourseLoadBySemester(String semester) {
+
+        return courseloadlist.get(semester);
+
+    }
+
+    public float getStudentTotalScore() {
+
+        float sum = 0;
+
+        for (CourseLoad cl : courseloadlist.values()) {
+            sum = sum + cl.getSemesterScore();
+
+        }
+        return sum;
+    }
+    
+    //generate a list of all courses taken so far (seetassignments) 
+    //from multiple semesters (course loads)
+    //from seat assignments we will be able to access the course offers
+    
+    public ArrayList<SeatAssignment> getCourseList() {
+        ArrayList temp2;
+        temp2 = new ArrayList();
+
+        for (CourseLoad cl : courseloadlist.values()) { //extract cl list as objects --ignore label
+            temp2.addAll(cl.getSeatAssignments()); //merge one array list to another
+        }
+        
+        return temp2;
+    
+    }
+    
+}
